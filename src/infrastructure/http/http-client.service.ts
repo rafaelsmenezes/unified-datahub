7d5cc017d14f4f682c0ffcac0ad12cc395719887
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2025-10-15 20:48:31 +0100

    feat: implement IHttpClientService interface in HttpClientService class

--- src/infrastructure/http/http-client.service.ts
+++ src/infrastructure/http/http-client.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosRequestConfig } from 'axios';
+import { IHttpClientService } from 'src/domain/http/http-client.interface';
 import { Readable } from 'stream';
 
 @Injectable()
-export class HttpClientService {
+export class HttpClientService implements IHttpClientService {
   async getStream(url: string, config?: AxiosRequestConfig): Promise<Readable> {
     const response = await axios.get<Readable>(url, {
       ...config,
       responseType: 'stream',
     });
     return response.data as unknown as Readable;
+  }
+
+  async get<T>(url: string): Promise<T> {
+    const response = await axios.get<T>(url);
+    return response.data;
   }
 }
