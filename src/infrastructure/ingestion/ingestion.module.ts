7c37322dcdeacbebca09892332194f251aec7089
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2025-10-15 20:49:25 +0100

    feat: create IngestionModule for managing ingestion services and dependencies

---
+++ src/infrastructure/ingestion/ingestion.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { IngestionService } from './ingestion.service';
+import { BatchSaverService } from './batch/batch-saver.service';
+import { StreamGeneratorService } from './stream/stream-generator.service';
+import { IBatchSaverServiceToken } from 'src/domain/ingestion/batch-saver.interface';
+import { IIngestionServiceToken } from 'src/domain/ingestion/ingestion.service.interface';
+import { IStreamGeneratorServiceToken } from 'src/domain/ingestion/stream-generator.interface';
+import { PersistenceModule } from '../persistence/persistence.module';
+import { HttpModule } from '../http/http.module';
+
+@Module({
+  imports: [PersistenceModule, HttpModule],
+
+  providers: [
+    {
+      provide: IIngestionServiceToken,
+      useClass: IngestionService,
+    },
+    {
+      provide: IBatchSaverServiceToken,
+      useClass: BatchSaverService,
+    },
+    {
+      provide: IStreamGeneratorServiceToken,
+      useClass: StreamGeneratorService,
+    },
+  ],
+  exports: [
+    IIngestionServiceToken,
+    IBatchSaverServiceToken,
+    IStreamGeneratorServiceToken,
+  ],
+})
+export class IngestionModule {}
