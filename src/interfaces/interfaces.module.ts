13cee41f57b663fed8224000d88f995316093276
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2025-10-16 14:25:47 +0100

    fix: replace ApiController with DataController in InterfacesModule

--- src/interfaces/interfaces.module.ts
+++ src/interfaces/interfaces.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ApiController } from './rest/api.controller';
+import { DataController } from './rest/data.controller';
 import { AdminController } from './rest/admin.controller';
 import { ApplicationModule } from '../application/application.module';
 
 @Module({
   imports: [ApplicationModule],
-  controllers: [ApiController, AdminController],
+  controllers: [DataController, AdminController],
 })
 export class InterfacesModule {}
