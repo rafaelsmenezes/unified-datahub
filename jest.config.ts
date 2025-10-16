1075fabfec62af7c888c415a35a8c5c01a60dea1
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2025-10-16 09:56:39 +0100

    chore: move Jest configuration to jest.config.ts for better organization

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': [
+      'ts-jest',
+      {
+        diagnostics: { ignoreCodes: [151002] },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/../src/$1',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
